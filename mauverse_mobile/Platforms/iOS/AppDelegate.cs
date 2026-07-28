732d6a34175687f26bdf1db70666208e4f348453
decoder-dev <decoder-dev@users.noreply.github.com>
2026-07-29 02:37:26 +0300

    Fix iOS Liquid Glass handlers

--- mauverse_mobile/Platforms/iOS/AppDelegate.cs
+++ mauverse_mobile/Platforms/iOS/AppDelegate.cs
@@ -1,8 +1,14 @@
 ﻿using Foundation;
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace mau.Platforms.iOS
 {
     [Register("AppDelegate")]
+    [SuppressMessage(
+        "Naming",
+        "CA1711:Identifiers should not have incorrect suffix",
+        Justification = "UIKit discovers the application delegate by its conventional registered name.")]
     public class AppDelegate : MauiUIApplicationDelegate
     {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
